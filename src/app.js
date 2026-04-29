b5a6aa4fc44da7e8aa71f5e522e9c8cdbcd7401f
AMNavinKumar2701 <ankireddymnaveenkumar@gmail.com>
2026-04-29 16:36:58 +0530

    feat: configure CORS for all origins

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
